79812424f6be994f0ef34c185a1894750f8eadbd
Lobanovk <38396223+Lobanovk@users.noreply.github.com>
2018-12-06 00:11:47 +0300

    --
    Разработка вывода товара на страницу product. Внедренна карусель на странице single_page. Сделан переход на страницу регистрации нового пользователя при выборе регистрации на странице checkout

--- js/users.js
+++ js/users.js
@@ -1,5 +1,8 @@
-
-function registration() {
+const user = {
+  /**
+   * Региструет нового пользователя
+   */
+  registration() {
     let user = {
       name: $('[name="name"]').val(),
       email: $('[name="email"]').val(),
@@ -18,8 +21,108 @@ function registration() {
         gender: user.gender,
         credit_card: user.credit_card,
       },
-      success: function () {
-        console.log("It's Ok");
+      success: function (success) {
+        console.log('Good');
       }
     })
-}
+  },
+
+  goToRegistred(){
+    if ( $('[name=a]:checked').val() === 'register'){
+      $(location).attr('href', 'http://localhost:63342/Shop%20Brand%20coursework/check_in.html?_ijt=vnpohfapueo5f6pre8r8cj7cqu');
+    }
+  },
+
+  /**
+   * Проверяет на совпадение почты и пароля из метаданных
+   */
+  init() {
+    $('.email-password').on('submit', e => user.autorizUser(e));
+    $('.check').on('click', '.button-text', function () {
+      user.goToRegistred();
+    });
+  },
+  /**
+   * Событие для проверки полей ввода
+   */
+  autorizUser(e) {
+    user.validate(function (isValid) {
+      if (!isValid){
+        e.preventDefault();
+        console.log('Fail');
+      } else {
+        console.log('Good');
+      }
+    });
+  },
+  /**
+   * Проверяет на совпадение email и password
+   * @returns {boolean} isValid Флаг, который показывает прошла проверка или нет
+   */
+  validate(flag) {
+    let userLogIn = {
+      email: $('[name = "email"]').val(),
+      password: $('[name = "password"]').val()
+    };
+    $.ajax({
+      url: 'http://localhost:3000/users',
+      type: 'GET',
+      success: function (users) {
+        console.log(users);
+        users.forEach(function (user) {
+          if (user.email === userLogIn.email) {
+            if (user.password === userLogIn.password) {
+              flag(true);
+            }
+          }
+          flag(false);
+        });
+      }
+    });
+  },
+};
+
+//   init() {
+//     $('.email-password').on('submit', e => user.autorizUser(e));
+//   },
+//   /**
+//    * Событие для проверки полей ввода
+//    */
+//   autorizUser(e) {
+//     user.validate();
+//     console.log(localStorage.getItem('userId'));
+//     if (!localStorage.getItem('userId')) {
+//       e.preventDefault();
+//       console.log('Fail');
+//     } else {
+//       console.log('Good');
+//     }
+//   },
+//
+//   /**
+//    * Проверяет на совпадение email и password
+//    */
+//   validate() {
+//     localStorage.clear();
+//     let userLogIn = {
+//       email: $('[name = "email"]').val(),
+//       password: $('[name = "password"]').val()
+//     };
+//     $.ajax({
+//       url: 'http://localhost:3000/users',
+//       type: 'GET',
+//       success: function (users) {
+//         console.log(users);
+//         users.forEach(function (user) {
+//           if (user.email === userLogIn.email) {
+//             if (user.password === userLogIn.password) {
+//               localStorage.setItem('userId', user.id);
+//             }
+//           }
+//         });
+//       }
+//     });
+//   },
+// };
+
+
